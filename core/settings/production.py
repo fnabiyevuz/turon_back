6748a56cc913ac21c3cc64b519aa58429c490ed9
fnabiyevuz <fnabiyevuz@gmail.com>
2025-09-08 16:54:23 +0500

    feat: docker

--- core/settings/production.py
+++ core/settings/production.py
@@ -14,20 +14,31 @@ USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ["https://example.com"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://mt.qabul-tuab.uz/",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
 
 ###################################################################
 # CORS
 ###################################################################
 
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_HEADERS = ["*"]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://mt.qabul-tuab.uz/",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
 
 REDIS_HOST = env.str("REDIS_HOST", "redis")
 REDIS_PORT = env.int("REDIS_PORT", 6379)
 REDIS_DB = env.int("REDIS_DB", 0)
-
 
 DATABASES = {
     "default": {
